d6f74955b7d71ff9650a73984df2bf698165e192
lofijay <anirudhyemekar2001@gmail.com>
2022-04-06 23:03:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <title>Corona Tracker</title>
 </head>
 <body>
-    <div class="container-fluid bg-dark text-center p-5 my-3">
+    <div class="container-fluid bg-hoppink text-center p-5 my-3">
         <h1>Corona Tracker</h1>
         <h5 class="text-muted">An opensource project to keep track of all the cases of covid 19 around the world.</h5>
     </div>
